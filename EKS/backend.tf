996d60c70d56f2d5394e2fb1a83a9c98b6cf3271
tavusB <ec2-user@ip-172-31-18-244.eu-west-2.compute.internal>
2020-05-12 19:33:31 +0000

    new qa

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform { 
 backend "s3" { 
 bucket = "tavus-terraform-eks" 
-key = "application/us-east-1/state/dev/infrastructure.tfstate.json" 
+key = "application/us-east-1/state/stage/infrastructure_oregon.tfstate" 
 region = "us-east-1" 
   } 
 } 
